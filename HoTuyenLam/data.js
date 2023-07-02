49c58b6df712e3699289b5d0f86b47573afc53e8
xuanviet73 <2012393@dlu.edu.vn>
2023-07-03 00:51:17 +0700

    VR

---
+++ HoTuyenLam/data.js
@@ -0,0 +1,101 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tuyenlam01",
+      "name": "tuyenlam01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.00431692827111263,
+          "pitch": 0.176453638313518,
+          "rotation": 0,
+          "target": "1-tuyenlam02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0059833051706625895,
+          "pitch": -0.47985504072753926,
+          "title": "Hồ Tuyền Lâm",
+          "text": "Hồ Tuyền Lâm là một hồ nước thuộc thành phố Đà Lạt tỉnh Lâm Đồng. Đây là hồ nước ngọt rộng nhất Ðà Lạt, với diện tích khoảng 320 ha, nằm cách trung tâm thành phố Đà Lạt 7 km và cách thác Ðatanla 2 km"
+        }
+      ]
+    },
+    {
+      "id": "1-tuyenlam02",
+      "name": "tuyenlam02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2031,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0376118829409835,
+          "pitch": 0.8934651122987738,
+          "rotation": 0,
+          "target": "0-tuyenlam01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07396332788934856,
+          "pitch": -0.3348261167208477,
+          "title": "Hồ Tuyền Lâm trên cao",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ data.js
@@ -0,0 +1,268 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hoxuanhuong0q",
+      "name": "HoXuanHuong0q",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 1.4532195333493023,
+        "pitch": 0.2908303967428907,
+        "fov": 1.4006798648781145
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.250560757149879,
+          "pitch": 0.31288998751977104,
+          "rotation": 0,
+          "target": "1-dapvit"
+        },
+        {
+          "yaw": 1.7520998046984797,
+          "pitch": 0.4012047203419229,
+          "rotation": 0,
+          "target": "2-thuyta01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0058499938231904025,
+          "pitch": 0.3135530900644419,
+          "title": "Quảng trường Lâm Viên",
+          "text": "Quảng trường Lâm Viên hay còn được du khách biết đến với tên gọi khác là Quảng trường Đà Lạt. Đây là một trong những địa điểm vui chơi thú vị, hấp dẫn nhất tại Đà Lạt mà bạn không thể nào bỏ qua.&nbsp;"
+        },
+        {
+          "yaw": 1.3515568988826487,
+          "pitch": 0.03236403914203123,
+          "title": "Khách sạn Dalat Palace",
+          "text": "Khách sạn Dalat Palace là một công trình kiến trúc kiểu Pháp. Khuôn viên của khách sạn rộng đến hơn 40 nghìn mét vuông, chung quanh là vườn hoa, thảm cỏ, rừng thông. Khách sạn có 43 phòng. Tất cả đều được trang bị thiết bị, đồ dùng sang trọng, nhiều phòng có lò sưởi."
+        },
+        {
+          "yaw": 3.1289611712067913,
+          "pitch": 0.17438957783580378,
+          "title": "Sân golf Đồi Cù",
+          "text": "Sân Golf Đồi Cù hay còn được gọi là sân Golf Đà Lạt hoặc Đà Lạt Palace Golf Club là một trong những địa điểm du lịch nổi tiếng nằm ngay trung tâm thành phố Đà Lạt kế bên Hồ Xuân Hương. Sân Golf tô điểm cho thành phố bằng màu xanh mướt trải dài như bất tận."
+        },
+        {
+          "yaw": -2.668831623247943,
+          "pitch": -0.030994261472603313,
+          "title": "Trường Đại học Đà Lạt",
+          "text": "Trường Đại học Đà Lạt (Dalat University) là một trường đại học công lập tại Việt Nam, đào tạo đa ngành, đa lĩnh vực, cung cấp nguồn nhân lực trình độ cao về Khoa học tự nhiên và Công nghệ, Khoa học xã hội và Nhân văn, và Kinh tế; Là trung tâm nghiên cứu khoa học - chuyển giao công nghệ phục vụ sự phát triển kinh tế-xã hội và hội nhập quốc tế."
+        }
+      ]
+    },
+    {
+      "id": "1-dapvit",
+      "name": "DapVit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0324679921948707,
+          "pitch": 0.2662103325472138,
+          "rotation": 0,
+          "target": "4-thanhthuy"
+        },
+        {
+          "yaw": 0.4071489576537157,
+          "pitch": -0.25566748646231474,
+          "rotation": 0,
+          "target": "0-hoxuanhuong0q"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.47636654184176663,
+          "pitch": 0.12181046854815669,
+          "title": "Khu vục đạp vit",
+          "text": ""
+        },
+        {
+          "yaw": -2.972848614908848,
+          "pitch": -0.0019123054509737614,
+          "title": "Coffee Thanh Thủy",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-thuyta01",
+      "name": "ThuyTa01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1128110206435657,
+          "pitch": 0.0178304986368385,
+          "rotation": 0,
+          "target": "3-thuyta02"
+        },
+        {
+          "yaw": 0.09797122173965889,
+          "pitch": -0.03533357568018758,
+          "rotation": 0,
+          "target": "0-hoxuanhuong0q"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.090552506292795,
+          "pitch": -0.44825873890282253,
+          "title": "Nhà hàng Thủy Tạ",
+          "text": "Thành phố Đà Lạt có Hồ Xuân Hương nổi tiếng với vẻ đẹp êm đềm hữu tình. Xung quanh hồ nổi lên một địa điểm ăn uống mà người dân địa phương ghé tới thường xuyên đó là nhà hàng Thủy Tạ Đà Lạt."
+        }
+      ]
+    },
+    {
+      "id": "3-thuyta02",
+      "name": "ThuyTa02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.28060272398080777,
+          "pitch": -0.03775662228202137,
+          "rotation": 1.5707963267948966,
+          "target": "2-thuyta01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-thanhthuy",
+      "name": "ThanhThuy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2662767080163118,
+          "pitch": -0.021822353596588684,
+          "rotation": 1.5707963267948966,
+          "target": "1-dapvit"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "HoXuanHuong",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
